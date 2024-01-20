7e9932fafc3b0e613db453802e125b9a8e98081c
Paul <pavelkarastoynov@gmail.com>
2024-01-20 14:52:01 +0200

    Version: 1.0

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ class Student:
     gender = "Male"
     education = "MKA STEP"
 
+    def __init__(self):
+        # print(id(self))
+        self.name = "Oleg"
+        self.age = 15
 
 st1 = Student()
+print(st1.name)
+print(st1.age)
+print(id(st1))
 st2 = Student()
 
-print(st1.group)
-print(st2.group)
+print (st1.group)
+print (st2.group)
